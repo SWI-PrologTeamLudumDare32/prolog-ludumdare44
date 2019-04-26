9526546ba9a5f93df6a865162bce56b4e8133ecb
Andrew Dougherty <andrewdo@frdcsa.org>
2019-04-26 05:30:33 -0400

    Some simple to make sure everything loads correctly.

--- scripts/start-larkc-client.sh
+++ scripts/start-larkc-client.sh
@@ -6,4 +6,4 @@
 
 # swipl -s $LD44ROOT/swipl-to-swipl/prolog/larkc_client.pl
 
-cd ../../prolog-ludumdare44/prolog && swipl -s psyche_server.pl # -g go 
+cd ../../prolog-ludumdare44/prolog && swipl -s psyche_server.pl
