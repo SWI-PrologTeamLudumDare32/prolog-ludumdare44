0bd8ff56fe342e51af89cf8536249eb924200dec
Anne Ogborn <annie66us@yahoo.com>
2019-04-26 23:58:10 -0700

    added session info to getMt

--- prolog/adventure_larkc_client.pl
+++ prolog/adventure_larkc_client.pl
@@ -1,5 +1,13 @@
+:- use_module(library(http/http_session)).
+
 getMt(Mt) :-
 	Mt = 'LD44-user_43-Mt'.
+getMt(Mt) :-
+	http_in_session(Session),
+	atomic_list_concat(['LD44-user_', Session, '-Mt'], Mt).
+getMt(_) :-
+	throw(error(resource_error(not_in_session), context(larkc_nanisearch_helper:getMt/1,
+						      'must have an http session to assert/retract'))).
 
 larkc_asserta(X) :-
 	getMt(Mt),
